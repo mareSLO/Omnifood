9f9a0ba67d5affa5c8aed75c0b0452de13f4ecd0
mareSLO <care000@gmail.com>
2015-10-13 17:40:29 +0200

    omnifood tutorial

---
+++ own/js/script.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+    
+    /* FOR THE STICKY NAVIGATION */
+    $('.js--features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '65px;'
+    });
+    
+    /* SCROLL FOR BUTTONS */
+    $('.js--full').click(function() {
+        $('html, body').animate({scrollTop: $('.js--pricing').offset().top}, 1000);
+    });
+    
+    $('.js--ghost').click(function() {
+        $('html, body').animate({scrollTop: $('.js--features').offset().top}, 1000);
+    });
+    
+    /* AUTO SMOOTH SCROOL FOR NAVBAR */
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html,body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+        });
+    });
+    
+    /* ANIMATIONS ON SCROLL */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    /* MOBILE NAVIGATION */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+        
+    });
+    /* GOOGLE MAPS */
+    var map = new GMaps({
+        div: '.map',
+        lat: 46.0243288,
+        lng: 14.4235,
+        zoom: 15
+    });
+    
+    map.addMarker({
+        lat: 46.0243288,
+        lng: 14.4235,
+        title: 'Brezovica',
+        infoWindow: {
+          content: '<p>Our Brezovica HQ!</p>'
+        }
+      });
+    
+});
